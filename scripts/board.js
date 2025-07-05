8011f8658ef471231d50fe2002f4b7a18fe3bc83
DeveloperRucel07 <tsafackrucel@gmail.com>
2025-07-05 18:05:10 +0200

    add font (Inter) and Summary page

--- scripts/board.js
+++ scripts/board.js
@@ -327,20 +327,3 @@ function removeHighlight(id) {
   document.getElementById(id).classList.remove("drag-area-highlight");
 }
 
-/**
- * Loads the navigation menu from "menu-bar.html" and inserts its HTML content
- * into the element with the ID "navbar". Logs the fetched HTML to the console.
- * If the fetch fails, logs an error message to the console.
- *
- * @function
- * @returns {void}
- */
-function loadMenu() {
-  fetch("menu-bar.html")
-    .then((response) => response.text())
-    .then((html) => {
-      console.log(html);
-      document.getElementById("navbar").innerHTML = html;
-    })
-    .catch((error) => console.error("Failed to load nav:", error));
-}
