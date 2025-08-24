0bf4613f3e2e09f540d42b55ec7d791c743bdd4c
Jeremy Nánási <Njeremy2002@gmail.com>
2025-08-24 12:57:12 +0200

    documentacion of help.js

--- scripts/help.js
+++ scripts/help.js
@@ -3,7 +3,7 @@
 * Goes to the last open website if there was a previous one. 
 */
 function goBack(){
-    if(navigation.canGoBack) {
-        history.back();
+    if(navigation.canGoBack) { // Checks whether the navigation has a previous page to return to
+        history.back(); // Reopen the last window
     }
 }
